245803834ee9889105364a34777892db76806c00
Hhamid97 <38023834+Hhamid97@users.noreply.github.com>
2018-05-14 23:10:58 +0600

    Add files via upload
    It's been tough

--- id_genC.h
+++ id_genC.h
@@ -4,6 +4,7 @@ class id_genC
 	int id;
 public:
 	id_genC() { id = 1600400; };
+	id_genC(int id_) { id = id_; }
 	int get_id() { int i = id; id++; return i; }
 	
 };
--- id_genE.h
+++ id_genE.h
@@ -4,6 +4,7 @@ class id_genE
 	int id;
 public:
 	id_genE() { id = 1700410; }
+	id_genE(int id_) { id = id_; }
 	int get_id() { int i = id; id++; return i; }
 	~id_genE();
 };
